8265e1761161628fa08e73a95def42d8d0c22115
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-12-04 14:19:06 -0500

    712601-1638645542

--- touch-block-time.py
+++ touch-block-time.py
@@ -4,10 +4,9 @@ import os
 import sys
 #sys.path.insert(0, os.path.abspath('/usr/local/lib/python3.9/site-packages'))
 import time
+import blockcypher
 millis = int(round(time.time() * 1000))
 seconds = int(round(time.time()))
-
-import blockcypher
 
 try:
     block_time = blockcypher.get_latest_block_height(coin_symbol='btc')
